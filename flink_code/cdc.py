3d804c15f76a25bde1ad24761507f04f99f84783
minhpn <minhpn@raffles.com.vn>
2025-01-15 09:40:03 +0700

    update code

--- flink_code/cdc.py
+++ flink_code/cdc.py
@@ -79,6 +79,7 @@ src_ddl = """
         'topic' = 'postgres.public.demo',
         'properties.bootstrap.servers' = 'broker-incluster:29092',
         'properties.group.id' = 'flink_connector',
+        'scan.startup.mode' = 'latest-offset',
         'format' = 'json'
     )
 """
