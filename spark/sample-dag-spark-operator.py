0d4b78553f75c85a968ba259b977775a0b99cbd0
minhpn <minhpn@raffles.com.vn>
2025-03-20 11:26:37 +0700

    update code

--- spark/sample-dag-spark-operator.py
+++ spark/sample-dag-spark-operator.py
@@ -19,14 +19,12 @@ with DAG(
    schedule_interval=timedelta(days=1),
    start_date=datetime(2022, 11, 17),
    catchup=False,
-   tags=['example'],
-   template_searchpath='/opt/airflow/dags/repo/spark'
+   tags=['example']
 ) as dag:
     t1 = SparkKubernetesOperator(
         task_id='n-spark-pi',
         trigger_rule="all_success",
-        depends_on_past=False,
-        application_file="sample-spark-pi.yaml",
+        application_file="repo/spark/sample-spark-pi.yaml",
         namespace="pm-spark",
         kubernetes_conn_id="k8s",
         do_xcom_push=True,
