a246639c997ec2cdb8cdf1195d5221453c8bc74f
minhpn <minhpn@raffles.com.vn>
2025-03-20 11:30:05 +0700

    update code

--- spark/sample-dag-spark-operator.py
+++ spark/sample-dag-spark-operator.py
@@ -13,20 +13,19 @@ default_args={
    'email_on_retry': False
 }
 with DAG(
-   'sample-dag-spark-operator',
-   default_args=default_args,
-   description='simple dag',
-   schedule_interval=timedelta(days=1),
-   start_date=datetime(2022, 11, 17),
-   catchup=False,
-   tags=['example']
+    'sample-dag-spark-operator',
+    default_args=default_args,
+    description='simple dag',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2025, 3, 20),
+    catchup=False,
+    tags=['example'],
+    template_searchpath='/opt/airflow/dags/repo/spark'
 ) as dag:
     t1 = SparkKubernetesOperator(
         task_id='n-spark-pi',
-        trigger_rule="all_success",
-        application_file="repo/spark/sample-spark-pi.yaml",
-        namespace="pm-spark",
-        kubernetes_conn_id="k8s",
+        application_file='sample-spark-pi.yaml',
+        namespace='pm-spark',
+        kubernetes_conn_id='k8s',
         do_xcom_push=True,
-        dag=dag
     )
